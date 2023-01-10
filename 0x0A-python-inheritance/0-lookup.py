79f97007c53d1861303f6e78b6cdc821bb777cc4
nazaek <chinazaekperejustice@gmail.com>
2023-01-10 04:06:40 +0000

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-   0-lookup.py -list of available attributes and methods of an object
-"""
+#0-lookup.py
 
 def lookup(obj):
     """This functions looks out for all attributes and methods of an object"""
